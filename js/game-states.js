1d0c512410beaa72a40eb70ffa372abd350c47f2
Axel Roussille Åberg <axelra82@gmail.com>
2020-11-05 22:47:22 +0100

    DRY

--- js/game-states.js
+++ js/game-states.js
@@ -1,18 +1,21 @@
 // Tuff non-luck!
 const gameOver = () => {
-	document.body.innerHTML = '<h1>GAME OVER</h1>';
 	disableButtons();
+	changeLayout('GAME OVER!', 'lose');
 }
 
 // Are you a prodigy or did you just cheat?
 const playerWins = () => {
 	disableButtons();
-	const gameHeader = gameEl.querySelector('h2');
-	gameHeader.classList.add('text-center');
-	gameHeader.innerHTML = 'YOU DID IT!';
-	letterPlaceholders.classList.add('success');
+	changeLayout('YOU DID IT!', 'success');
 }
 
+const changeLayout = (header, listClass) => {
+	const gameHeader = gameEl.querySelector('h2');
+	gameHeader.classList.add('text-center');
+	gameHeader.innerHTML = header;
+	letterPlaceholders.classList.add(listClass);
+}
 // No more buttons for you!
 const disableButtons = () => {
 	const letterButtons = document.querySelectorAll('ul#letters li button');
