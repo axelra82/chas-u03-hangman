1cf3be83f39eaf3366735839344a3f8a140a6c15
Axel Roussille Åberg <axelra82@gmail.com>
2020-11-05 16:01:14 +0100

    Add class to secret list item rather than initial placeholder underscore

--- js/init.js
+++ js/init.js
@@ -23,7 +23,7 @@ const lettersArray = randomWord.split('');
 const letterPlaceholders = document.querySelector('ul#secretWord');
 lettersArray.forEach(() => {
 	const liEl = document.createElement('li');
-	liEl.innerHTML = '_';
+	liEl.classList.add('secret');
 	letterPlaceholders.appendChild(liEl);
 });
 
@@ -53,45 +53,62 @@ alphabet.forEach(letter => {
 
 	// Add click event listner for each letter
 	buttonEl.addEventListener('click', () => {
+		verify(buttonEl, letter);
+	});
 
-		// Button is clicked, don't be a jerk!
-		// Disable and remove listener
-		buttonEl.setAttribute('disabled', 'true');
-		buttonEl.removeEventListener;
+	document.addEventListener('keydown', pressed => {
+		const keyValue = pressed.key;
+		const isInAlphabet = alphabet.includes(keyValue);
 
-		// Check if clicked letter exists in word letter array
-		if (lettersArray.includes(letter)) {
-
-			const placeHolderItems = letterPlaceholders.querySelectorAll('li');
-			lettersArray.forEach((l, i) => {
-				if (l === letter) {
-					// YEAY!
-					// Increase correct count by one (1)
-					correctCount++;
-					placeHolderItems[i].innerHTML = letter;
-				}
-			});
-
-			if (correctCount === lettersArray.length) {
-				youWin();
-			}
-
-			console.log(`Keep going! You have ${maxClick - wrongCount} chances left`);
-		} else {
-			// No dice!
-			// Increase wrong count by one (1)
-			wrongCount++;
-			if (wrongCount === maxClick) {
-				gameOver();
-			} else if (wrongCount < maxClick - 1) {
-				console.log(`Ooops! That's ok... You still have ${maxClick - wrongCount} chances left`);
-			} else {
-				console.log(`Ooh.. Last chance!`);
-			}
+		if (isInAlphabet) {
+			verify(buttonEl, letter);
 		}
-
 	});
 });
+
+// Check whats happening
+const verify = (button, letter) => {
+	// Button is clicked, don't be a jerk!
+	// Disable and remove listener
+	button.setAttribute('disabled', 'true');
+	button.removeEventListener;
+
+	// Check if clicked letter exists in word letter array
+	if (lettersArray.includes(letter)) {
+
+		const placeHolderItems = letterPlaceholders.querySelectorAll('li');
+		lettersArray.forEach((l, i) => {
+			if (l === letter) {
+				// YEAY!
+				// Increase correct count by one (1)
+				correctCount++;
+
+				// Some DOM manipulation
+				const placeHolderItem = placeHolderItems[i];
+				placeHolderItem.classList.remove('secret')
+				placeHolderItem.classList.add('correct')
+				placeHolderItem.innerHTML = letter;
+			}
+		});
+
+		if (correctCount === lettersArray.length) {
+			youWin();
+		}
+
+		console.log(`Keep going! You have ${maxClick - wrongCount} chances left`);
+	} else {
+		// No dice!
+		// Increase wrong count by one (1)
+		wrongCount++;
+		if (wrongCount === maxClick) {
+			gameOver();
+		} else if (wrongCount < maxClick - 1) {
+			console.log(`Ooops! That's ok... You still have ${maxClick - wrongCount} chances left`);
+		} else {
+			console.log(`Ooh.. Last chance!`);
+		}
+	}
+}
 
 // Tuff non-luck!
 const gameOver = () => {
